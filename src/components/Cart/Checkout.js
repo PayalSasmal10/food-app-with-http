94e0d288b61513479f6a88d976caa302a70437e6
Payal Sasmal <sasmalpayal@gmail.com>
2023-01-06 21:29:34 +0530

    adding form

---
+++ src/components/Cart/Checkout.js
@@ -0,0 +1,27 @@
+import classes from './Cart/Checkout.module.css';
+
+const Checkout = (props) => {
+    return (
+        <form>
+            <div className={classes.control}>
+                <label htmlFor='name'>Your Name</label>
+                <input type='text' id='name' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='street'>Street</label>
+                <input type='text' id='street' />
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='postal'>Postal Code</label>
+                <input type='number' id='postal'/>
+            </div>
+            <div className={classes.control}>
+                <label htmlFor='city'>City</label>
+                <input type='text' id='city'/>
+            </div>
+            <button>Confirm</button>
+        </form>
+    );
+};
+
+export default Checkout;
--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -12,7 +12,7 @@ const AvailableMeals = () => {
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch("https://react-https-2fdcb-default-rtdb.asia-southeast1.firebasedatabase.app/meals");
+      const response = await fetch("https://react-https-2fdcb-default-rtdb.asia-southeast1.firebasedatabase.app/meals.json");
       
       if(!response.ok){
         throw new Error("Something went wrong!");
